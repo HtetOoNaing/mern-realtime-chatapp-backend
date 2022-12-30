3c872700c72ed48c63147d0e570ec6b91e502aff
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2022-12-30 22:22:50 +0630

    add sendMessage method in messageController

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,6 +1,37 @@
 const asyncHandler = require("express-async-handler");
+const Chat = require("../models/chat");
+const Message = require("../models/message");
+const User = require("../models/user");
 
-const sendMessage = asyncHandler(async (req, res) => {});
+const sendMessage = asyncHandler(async (req, res) => {
+  const { content, chatId } = req.body;
+
+  if (!content || !chatId) {
+    console.log("Invalid data passed into request");
+    return res.status(400);
+  }
+  let newMessage = {
+    sender: req.user._id,
+    content,
+    chat: chatId,
+  };
+  try {
+    let message = await Message.create(newMessage);
+    message = await message.populate("sender", "name pic");
+    message = await message.populate("chat");
+    message = await User.populate(message, {
+      path: "chat.users",
+      select: "name pic email",
+    });
+    await Chat.findByIdAndUpdate(req.body.chatId, {
+      latestMessage: message,
+    });
+    res.json(message);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
 
 module.exports = {
   sendMessage,
