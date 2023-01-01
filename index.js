d75f25f9e963643bbd9d5be87f5e0d0c15c4b1cc
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-01-01 23:41:20 +0630

    add new message socket

--- index.js
+++ index.js
@@ -44,4 +44,12 @@ io.on("connection", (socket) => {
     socket.join(room);
     console.log("user joined room: " + room);
   });
+  socket.on("new message", (newMessageReceived) => {
+    let chat = newMessageReceived.chat;
+    if (!chat.users) return console.log("chat.users not defined");
+    chat.users.forEach((user) => {
+      if (user._id === newMessageReceived.sender._id) return;
+      socket.in(user._id).emit("message received", newMessageReceived);
+    });
+  });
 });
